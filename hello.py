2503cbde7b9a3296cc4c3bcfcedc612be19a1356
Oliver Hanna <oliver.hanna@colorado.edu>
2017-05-09 15:34:51 -0600

    Changed hello function name

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 #https://github.com/softwaresaved/build_and_test_examples/blob/master/travis/HelloWorld.md
 
-def hello():
+def talk():
   print "Hello world from Travis CI"
   return "Hello world from Travis CI"
 if __name__ == "__main__":
-    hello()
+    talk()
